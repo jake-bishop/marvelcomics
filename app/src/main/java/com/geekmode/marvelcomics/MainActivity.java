0386485414db7e01ade52e673fdaf7f57877e3d6
Jake Bishop <jake.bishop@geekmode.com>
2016-09-13 09:10:19 -0500

    Move text to strings.xml. Add scrollview for text. fix layouts.
    Signed-off-by: Jake Bishop <jake.bishop@geekmode.com>

--- app/src/main/java/com/geekmode/marvelcomics/MainActivity.java
+++ app/src/main/java/com/geekmode/marvelcomics/MainActivity.java
@@ -2,6 +2,7 @@ package com.geekmode.marvelcomics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,18 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView bishopCardTextView = (TextView)findViewById(R.id.bishop_card_text);
-        bishopCardTextView.setText(
-                "The mutant known only as Bishop was born in the 21st Century A.D. of alternate " +
-                        "timeline Earth-1191.\n" +
-                        "Bishop can absorb most types of energy, including magic and psychic, " +
-                        "directed toward him. The nature of his powers makes it difficult to " +
-                        "damage him with energy-based attacks.\n" +
-                        "Bishop first appears as a member of Xavier's Security Enforcers " +
-                        "(initially called the Xavier School Enforcers), a mutant police force " +
-                        "from a dystopian future of the Marvel Universe. He travels to the 20th " +
-                        "century and joins the X-Men, a team he knew only as legends."
-        );
     }
 }
